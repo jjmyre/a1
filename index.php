568bc9e82787c819065dcb67460910f601d12134
Justin Myre <justin.myre@gmail.com>
2017-02-09 04:25:40 -0800

    HTML structure and image added

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Justin Myre</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+	<div class="wrapper">
+    	<h1>Justin Myre</h1>
+    	<img id="me" src="images/me.jpg" alt="Justin and Sherman the Beagle"/>
+    	<h2>About Me</h2>
+        <p>well, hello</p>
+        <h2>Quotes</h2>    
+    </div>
+</body>
+
+</html>
+
