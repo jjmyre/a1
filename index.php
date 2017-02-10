4de8164e2c1b3bbd1138efd3369ed1a8e128e095
Justin Myre <justin.myre@gmail.com>
2017-02-09 17:25:51 -0800

    Production ready. PHP quote generator working, all content set.

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require('quotes.php'); ?>
 <!DOCTYPE html>
 <html>
 
@@ -12,13 +13,18 @@
 
 <body>
 	<div id="wrapper">
-    	<h1>Justin Myre</h1>
-    	<img id="me" src="images/me.jpg" alt="Justin with Sherman the Beagle"/>
-    	<h2>About Me</h2>
-        <p>I was born and partially raised in Montana, but moved to southeastern Washington when I was in secondary school. I graduated from Montana State University in Bozeman with a BA in Media Production and worked on films and commercials in Los Angeles soon after. When LA grew tiring for me (and boy did it ever), I retreated back to my hometown in Washington state where I currently reside. I still work in media, though on a much smaller scale (local news stations, freelance, etc). I enjoy the great outdoors and feel blessed for living in the Pacific Northwest where there is plenty of roaming space and scenery to explore. In recent years I've begun to shift my attention more toward web design and development and have found that several of my skill sets are transferrable and applicable. What was once a passing interest eventually led me to pursue an MLA in Digital Media Design through the Harvard Extension School.</p>
+        <header>
+    	   <h1>Justin Myre</h1>
+        </header>
+    	
+        <img id="me" src="images/me.jpg" alt="Justin with Sherman the Beagle"/>
+    	
+        <h2>About Me</h2>
+        <p>I was born and partially raised in Montana state, but moved to southeastern Washington when I was in secondary school. I graduated from Montana State University in Bozeman with a BA in Media Production and began working on films and commercials in Los Angeles soon after graduating. When LA grew tiring for me (and boy did it ever), I retreated back to my hometown in Washington state where my wife and I currently reside. I still work in media, though on a much smaller scale (local news, events, freelance, etc). I enjoy the great outdoors and feel blessed for living in the Pacific Northwest where there is plenty of roaming space and scenery to explore. In recent years I've begun to shift my attention toward web design and development and have found that several of my skill sets are transferrable and applicable. What was once a passing interest eventually convinced me to pursue an MLA in Digital Media Design through the Harvard Extension School.</p>
      
         <h2>Random Quotes</h2>
         <blockquote>
+        <?php echo $randomQuote; ?>
         </blockquote>    
     </div>
 </body>
---
+++ quotes.php
@@ -0,0 +1,15 @@
+<?php
+	$randomQuotes = [
+		"\"I am at the moment writing a lengthy indictment against our century. When my brain begins to reel from 
+		my literary labors, I make an occasional cheese dip.\" - John Kennedy Toole, A Confederacy of Dunces",
+	
+		"\"Always remember that you are absolutely unique. Just like everyone else.\" - Margaret Mead",
+
+		"\"Laugh, and the world laughs with you. Weep, and you weep alone.\" - Ella Wheeler Wilcox, Poems of Passion",
+		
+		"\"When you come to a fork in the road, take it.\" - Yogi Berra",
+	];
+
+	$quoteNumber = rand(0,3);
+	$randomQuote = $randomQuotes[$quoteNumber];
+?>
