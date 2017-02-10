7862160e4b1916c9f44709a408cff07a8df9f56f
Justin Myre <justin.myre@gmail.com>
2017-02-09 16:32:13 -0800

    Added styling, structure is set. Awaiting Bio content and quotes.

--- index.php
+++ index.php
@@ -6,15 +6,20 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="icon" type="image/png" href="images/favicon.png"/>
+    <link rel="stylesheet" media="screen" href="https://fontlibrary.org/face/jellee-typeface" type="text/css"/>
 </head>
 
 <body>
-	<div class="wrapper">
+	<div id="wrapper">
     	<h1>Justin Myre</h1>
-    	<img id="me" src="images/me.jpg" alt="Justin and Sherman the Beagle"/>
+    	<img id="me" src="images/me.jpg" alt="Justin with Sherman the Beagle"/>
     	<h2>About Me</h2>
-        <p>well, hello</p>
-        <h2>Quotes</h2>    
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in pulvinar libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in pulvinar libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in pulvinar libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in pulvinar libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <h2>Random Quotes</h2>
+        <blockquote>
+        </blockquote>    
     </div>
 </body>
 
